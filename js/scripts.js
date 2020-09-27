20dc7498df66128c92784899b762632079ce47f9
joshuaiyumva <joshuaiyumva@gmail.com>
2020-09-27 22:59:40 +0200

    add icons on male and female button

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,8 @@
 var akanMale = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var akanFemale = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-function openForm(){
-    document.getElementById('maleForm').style.display = 'block';
-}
 function is_date(){
     var days = document.getElementById("bDate").value;
     var inserted_Date = days.split("/");
-
     var dd = parseInt(inserted_Date[1]);
     var mm = parseInt(inserted_Date[0]);
     var yyyy = parseInt(inserted_Date[2]);
