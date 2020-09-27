0c985eb77bd3c80ac71dff39e1cfa2708a58a314
joshuaiyumva <joshuaiyumva@gmail.com>
2020-09-27 18:48:04 +0200

    intergrate JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,36 @@
-var is_date = is_weekend();
+
+var male = document.getElementById('maleForm');
+var female = document.getElementById('femaleForm');
 var akanMale = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var akanFemale = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-var monthNames = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE",
-    "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"];
-var days = ['SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY'];
-var date1 = getElementById("DOB").value;
-var is_weekend = function (date1) {
-    var dt = new Date(date1);
-    if (dt.getDay() != 0 || dt.getDay() != 7) {
-        return "Invalid day";
+
+/*window.onclick = function (event) {
+    if ((event.target == male) || (event.target == female)) {
+        male.style.display = "none";
     }
-    else if (dt.getDay() == 0 || dt.getDay() == 7) {
-        return dt.getDay();
+}*/
+function is_date(){
+    var days = document.getElementById("bDate").value;
+    var inserted_Date = days.split("/");
+    var dd = parseInt(inserted_Date[1]);
+    var mm = parseInt(inserted_Date[0]);
+    var yyyy = parseInt(inserted_Date[2]);
+
+    var cc = ((yyyy-1)/100+1);
+    var birthCalc =(cc / 4 - 2 * cc - 1 + 5 * yyyy / 4 + 26 * (mm + 1) / 10 +dd) % 7;
+    document.getElementById("display").innerHTML = Math.round(birthCalc);
+    var gender = document.querySelector('input[name="genderBtn"]:submit').value;;
+    if(gender === "Male"){
+        /*console.log(akanMale[Math.round(birthCalc)]);*/
+        document.write.innerHTML("isAkan") = akanMale[Math.round(birthCalc)];
     }
-}
-function gender_check(){
-    var is_male = document.getElementById("gender_male").value;
-    var is_female = document.getElementById("gender_female").value;
-    if (is_date == false )
-    {
-        if(is_male == false && is_female == false){
-        alert('Please fill in all elements First');
-        return false;
-        }
-        else if(is_male ==true || is_female == true){
-            alert('Something is missing')
-            return false;
-            }
-        else
-            return true;
+    else if(gender === "Female"){
+        /*console.log(akanFemale[Math.round(birthCalc)]);*/
+        document.write.innerHTML("isAkan") = akanFemale[Math.round(birthCalc)];
     }
+    else{
+        alert("Invalid");
+        document.getElementsByName("mainPage");
+    }
+
 }
